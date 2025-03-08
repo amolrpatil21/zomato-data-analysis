60bf656e2713295ea7eaf1a8ba8a09e379c36b27
amolrpatil21 <amolpatil0625@gmail.com>
2025-03-08 19:28:08 +0530

    check the data is not include null values in the data set

---
+++ datacleaning and display.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+
+# Display the data
+
+zomato_data=pd.read_csv('zomato-data-analysis/Zomato data .csv')
+
+#print(zomato_data)       he is printing the data
+
+#display the rate column
+zomato_data = zomato_data[zomato_data['rate'].str.contains(r'\d', na=False)] 
+zomato_data['rate'] = zomato_data['rate'].str.split('/').str[0].astype(float) 
+
+print(zomato_data['rate'])
+print(f"the  sum of null value is {zomato_data['rate'].isnull().sum()}")
--- displaydata.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as np
-
-# Display the data
-
-data=pd.read_csv('zomato-data-analysis/Zomato data .csv')
-
-print(data)
